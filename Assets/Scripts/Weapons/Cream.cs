0930856cd418846301ade4dd018863cbaf7348e5
Rafinha <137532550+Rafinha-uwu@users.noreply.github.com>
2025-06-19 02:01:36 +0100

    Cream

--- Assets/Scripts/Weapons/Cream.cs
+++ Assets/Scripts/Weapons/Cream.cs
@@ -6,6 +6,7 @@ public class Cream : MonoBehaviour
     [SerializeField] private bool Zombies = true;
 
     [SerializeField] private GameObject GroundCream;
+        private Vector3 CreamLocation;
 
     private AudioSource _audioSource;
     [SerializeField] private AudioClip cheese_sound;
@@ -18,6 +19,28 @@ public class Cream : MonoBehaviour
         _audioSource = GetComponent<AudioSource>();
         part = GetComponentInChildren<ParticleSystem>();
         collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Zombie") && Zombies)
+        {
+            CreamLocation = other.transform.position;
+            CreamLocation.y -= 1.1f;
+            Instantiate(GroundCream, CreamLocation, GroundCream.transform.rotation);
+
+            PlayStickySound();
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Ground"))
+        {
+            CreamLocation = transform.position;
+            CreamLocation.y -= 0f;
+            Instantiate(GroundCream, CreamLocation, GroundCream.transform.rotation);
+
+            PlayStickySound();
+            Destroy(gameObject);
+        }
     }
 
     public void OnParticleCollision(GameObject other)
