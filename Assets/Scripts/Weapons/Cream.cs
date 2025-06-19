d969426f8eef134339a36b33a550e89a15e516a8
PedroC55 <plbc@ua.pt>
2025-06-19 02:09:22 +0100

    Update sounds of explosion and cheese

--- Assets/Scripts/Weapons/Cream.cs
+++ Assets/Scripts/Weapons/Cream.cs
@@ -7,15 +7,12 @@ public class Cream : MonoBehaviour
 
     [SerializeField] private GameObject GroundCream;
 
-    private AudioSource _audioSource;
-    [SerializeField] private AudioClip cheese_sound;
 
     private ParticleSystem part;
     private List<ParticleCollisionEvent> collisionEvents;
 
     void Start()
     {
-        _audioSource = GetComponent<AudioSource>();
         part = GetComponentInChildren<ParticleSystem>();
         collisionEvents = new List<ParticleCollisionEvent>();
     }
@@ -32,21 +29,14 @@ public class Cream : MonoBehaviour
             {
                 collisionPos.y -= 1.1f;
                 Instantiate(GroundCream, collisionPos, GroundCream.transform.rotation);
-                PlayStickySound();
             }
             else if (other.CompareTag("Ground"))
             {
                 Instantiate(GroundCream, collisionPos, GroundCream.transform.rotation);
-                PlayStickySound();
             }
 
             Destroy(gameObject.transform.parent.gameObject);
         }
     }
 
-    private void PlayStickySound()
-    {
-        _audioSource.clip = cheese_sound;
-        _audioSource.Play();
-    }
 }
